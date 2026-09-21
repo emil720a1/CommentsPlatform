7e64a42addf84bcd6859f33971d6013faae55e2e
emil720a1 <emil720apro@gmail.com>
2026-09-21 23:40:36 +0300

    feat/17/configure-persistence-mappings

--- src/CommentsPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/CommentsPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
+using CommentsPlatform.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace CommentsPlatform.Infrastructure.Persistence;
 
-public class ApplicationDbContext : DbContext
+public sealed class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<Attachment> Attachments => Set<Attachment>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
